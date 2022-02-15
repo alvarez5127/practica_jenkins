6896feaca035e60e9525859ddf506bb81da44c1e
Chumbi <alvarez.5127@gmail.com>
2022-02-15 13:07:11 -0500

    cambio texto en clase

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se ha hecho 3");
+		System.out.println("La conexión con Github se ha hecho 4");
 	}
 }
