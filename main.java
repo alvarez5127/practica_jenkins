4194ca8d10550edb7337510a69aee17950020ab5
Chumbi <alvarez.5127@gmail.com>
2022-02-15 13:37:59 -0500

    cambio texto para error prueba envio correo

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se ha realizado 9")
+		System.out.println("La conexión con Github se ha realizado 10")
 	}
 }
